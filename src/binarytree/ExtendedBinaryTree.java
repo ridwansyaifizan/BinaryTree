13f90e7c2b75795823e1a1db9e8a477a69cff329
ridwansyaifizan <syaifizanridwan@gmail.com>
2017-10-31 19:49:26 +0700

    BinaryTree

---
+++ src/binarytree/ExtendedBinaryTree.java
@@ -0,0 +1,7 @@
+package BinaryTree;
+
+public interface ExtendedBinaryTree {
+    public boolean compareTrees(BinaryTreeNode a, BinaryTreeNode b);
+    public Object clone();
+    public void swapSubtrees(final BinaryTreeNode root);
+}
